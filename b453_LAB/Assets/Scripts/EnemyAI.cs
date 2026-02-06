dd15695da097cb2e47756a920b8c4bb0820b5a58
CaptainCookii <evanlucas05@outlook.com>
2026-02-05 22:08:48 -0500

    Week 4 Section 2

--- b453_LAB/Assets/Scripts/EnemyAI.cs
+++ b453_LAB/Assets/Scripts/EnemyAI.cs
@@ -7,9 +7,14 @@ public class EnemyAI : MonoBehaviour
     [SerializeField] private float speed = 5f;
     [SerializeField] private float stoppingDistance = 5f;
     [SerializeField] private float attackRange = 5f;
+    [SerializeField] private float sightRange = 15f;
 
     private NavMeshAgent agent;
     private Transform player;
+    private Transform waypointOne;
+    private Transform waypointTwo;
+    private Transform currentPoint;
+    private float timePassed;
 
     void Awake()
     {
@@ -18,18 +23,33 @@ public class EnemyAI : MonoBehaviour
         agent.stoppingDistance = stoppingDistance;
 
         player = GameObject.FindGameObjectWithTag("Player").transform;
+        waypointOne = GameObject.FindGameObjectWithTag("waypointOne").transform;
+        waypointTwo = GameObject.FindGameObjectWithTag("waypointTwo").transform;
+        currentPoint = waypointOne;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Vector3.Distance(transform.position, player.position) <= agent.stoppingDistance)
+        RaycastHit sight;
+        Debug.DrawRay(transform.position, transform.forward, Color.blue, 0.1f);
+        if (Physics.Raycast(transform.position, transform.forward, out sight, sightRange))
+        {
+            if (sight.collider.CompareTag("Player"))
+            {
+                Move(player);
+            }
+        }
+        
+        if (Vector3.Distance(transform.position, player.position) <= agent.stoppingDistance)
         {
             Attack();
-        } else
-        {
-            Move();
         }
+        else
+        {
+            Patrol();
+        }
+
     }
 
     void Attack()
@@ -47,8 +67,38 @@ public class EnemyAI : MonoBehaviour
         }
     }
 
-    private void Move()
+    private void Move(Transform target)
     {
-        agent.SetDestination(player.position);
+        agent.SetDestination(target.position);
+    }
+
+    private void Patrol()
+    {
+        if (Vector3.Distance(transform.position, currentPoint.position) <= agent.stoppingDistance)
+        {
+            Sweep();
+            
+        } else
+        {
+            Move(currentPoint);
+        }
+    }
+
+    private void Sweep()
+    {
+        transform.Rotate(Vector3.up * 65f * Time.deltaTime);
+        timePassed += Time.deltaTime;
+        if (timePassed >= 7f)
+        {
+            if (currentPoint == waypointOne)
+            {
+                currentPoint = waypointTwo;
+            }
+            else
+            {
+                currentPoint = waypointOne;
+            }
+            timePassed = 0;
+        }
     }
 }
